a280ea16ad47c8325c5ece07677aae811360bed7
NachtHemd <nachthemd@localhost.localdomain>
2019-07-20 19:39:29 +0200

    Added t1

---
+++ HW03/task1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "output.h"
+
+int strlength (char string[]) {
+	int i = 0;
+	while (string[i] != '\0') {
+		++i;
+	}
+	return i;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Only one command line argument permitted.\n");
+		exit(1);
+	}
+	char *input = argv[1];
+	outputT1(strlength(input));
+	return 0;
+}
